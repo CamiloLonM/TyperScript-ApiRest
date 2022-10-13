cae80ab8c4e8a24f12123b94cdc5f15113cd05e6
camilo londoño <cl.mon9566@gmail.com>
2022-10-13 17:14:19 -0500

    controllers

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,8 @@ import db from "./config/mongo";
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.use(cors({}));
+app.use(cors());
+app.use(express.json());
 app.use(router);
 db().then(() => console.log("Connected to the database"));
 
