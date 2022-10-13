8e96c63231056cc4263b338acba1137a11d1f61d
Londoño <98825779+CamiloLonM@users.noreply.github.com>
2022-10-12 20:25:20 -0500

    adding dynamic loader (#1)

---
+++ src/routes/index.ts
@@ -0,0 +1,26 @@
+import { Router } from "express";
+import { readdirSync } from "fs";
+
+const PATH_ROUTER = `${__dirname}`;
+const router = Router();
+
+const cleanFileName = (fileName: string) => {
+  const file = fileName.split(".").shift();
+  return file;
+};
+
+// ( Cargador dinamico de rutas) Scanea cuantos archivos que existen dentro de ese directorio
+// OJO!!! El nombre del archivo es el nombre de la ruta
+readdirSync(PATH_ROUTER).filter((fileName) => {
+  const cleanName = cleanFileName(fileName);
+  if (cleanName !== "index") {
+    // Importacion dinamica
+    import(`./${cleanName}`).then((moduleRouter) => {
+      console.log(`The route is being loaded.... ${cleanName}`); // rutas
+
+      router.use(`/${cleanName}`, moduleRouter.router);
+    });
+  }
+});
+
+export { router };
